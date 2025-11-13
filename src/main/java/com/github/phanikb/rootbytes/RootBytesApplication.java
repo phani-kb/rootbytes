adf0ecec9d2e955db9ea0514175b614636c84a61
Phani K <192951055+phani-kb@users.noreply.github.com>
2025-11-12 22:35:07 -0500

    Initialize project structure

---
+++ src/main/java/com/github/phanikb/rootbytes/RootBytesApplication.java
@@ -0,0 +1,12 @@
+package com.github.phanikb.rootbytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootBytesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootBytesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/phanikb/rootbytes/RootBytesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.phanikb.rootbytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootBytesApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
