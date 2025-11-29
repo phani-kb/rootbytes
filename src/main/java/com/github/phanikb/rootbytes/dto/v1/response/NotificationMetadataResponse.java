254b96d518786b33d4e70caa9db5c1ca48720697
Phani K <192951055+phani-kb@users.noreply.github.com>
2025-11-28 20:51:43 -0500

    Add metadata response

---
+++ src/main/java/com/github/phanikb/rootbytes/dto/v1/response/NotificationMetadataResponse.java
@@ -0,0 +1,24 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2025 RootBytes. All Rights Reserved.
+ * Author: Phani K
+ */
+
+package com.github.phanikb.rootbytes.dto.v1.response;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationMetadataResponse {
+    private String entityType;
+    private UUID entityId;
+    private String actionUrl;
+}
